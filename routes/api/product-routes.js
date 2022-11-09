863e3ae20796148e8e16553d7975c2258da7ea0b
sangki810 <113147507+sangki810@users.noreply.github.com>
2022-11-09 05:11:20 -0500

    debugged routes files

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -54,10 +54,11 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
   /* req.body should look like this...
     {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
+      "product_name": "Basketball",
+      "price": 200.00,
+      "stock": 3,
+      "category_id": 1
+      "tagIds": [1, 2, 3, 4]
     }
   */
   Product.create(req.body)
@@ -86,10 +87,10 @@ router.post('/', (req, res) => {
 router.put('/:id', (req, res) => {
   /* req.body should look like this...
     {
-      product_name: "Soccer",
-      price: 300.00,
-      stock: 5,
-      tagIds: [1, 4]
+      "product_name": "Soccer",
+      "price": 300.00,
+      "stock": 5,
+      "tagIds": [1, 4]
     }
   */
   // update product data
@@ -134,6 +135,20 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(productData => {
+      if (!productData) {
+        res.status(404).json({message: 'No product with this id can be found'});
+        return;
+      }
+        res.json(productData)
+  }).catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
 });
 
 module.exports = router;
